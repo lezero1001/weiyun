197b8c85ad716f75fa8363019d709401cc6af2ea
署zzg <1450465633@qq.com>
2019-11-21 20:35:58 +0800

    短信模块调整

---
+++ weiyun-sms/src/main/java/com/wy/WySmsService.java
@@ -0,0 +1,11 @@
+package com.wy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WySmsService {
+    public static void main(String[] args) {
+        SpringApplication.run(WySmsService.class, args);
+    }
+}
