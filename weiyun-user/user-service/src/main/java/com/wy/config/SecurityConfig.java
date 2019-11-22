e313cda4dca8dcfcdd292d747770b5ec50720f06
yangyang <1023861154@qq.com>
2019-11-22 21:40:12 +0800

    权限测试bug

--- weiyun-user/user-service/src/main/java/com/wy/config/SecurityConfig.java
+++ weiyun-user/user-service/src/main/java/com/wy/config/SecurityConfig.java
@@ -18,19 +18,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserService userService;
+    private UserDetailsService userService;
 
     //对要访问的页面资源进行授权限制
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
         //链式调用/编程--->构建器模式.
-        http.authorizeRequests() //对页面|资源请求进行认证授权操作
-                .antMatchers("/css/**", "/index")
+        http.csrf().disable().
+                    authorizeRequests()
+                 //对页面|资源请求进行认证授权操作
+                .antMatchers("/css/**", "/index","/login")
                 .permitAll()
                 .antMatchers("/user/**")
                 //.hasAuthority("ROLE_USER");
-                .hasAnyAuthority("CAROWNER","GOODOWNER")
+                .hasAnyAuthority("ROLE_CAROWNER","ROLE_GOODOWNER")
                 .antMatchers("/blogs/**")
                 //.hasRole("USER");
                 .hasAnyRole("GENERALADMIN", "SUPERADMIN")
@@ -43,9 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .accessDeniedPage("/401")
                 .and()
                 .logout() //对退出进行设置
-                .logoutSuccessUrl("/")
-                .and()
-                .csrf().disable();
+                .logoutSuccessUrl("/");
+
 
     }
 
@@ -62,14 +63,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //BCryptPasswordEncoder:属于对称加密算法.des/3des/bcrypt等都是对称加密.
         //注意:新版本的spring-security中,必须明确指明要用的密码加密方案,否则:
         //IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
-//        auth.inMemoryAuthentication()
-//                .passwordEncoder(new BCryptPasswordEncoder())
-//                .withUser("syc")
-//                .password(new BCryptPasswordEncoder().encode("123"))
-//                .roles("USER");
+        auth.inMemoryAuthentication().
+                passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("weiyun")
+                .password(new BCryptPasswordEncoder().encode("123456"))
+                .authorities("ROLE_CAROWNER");
 
         //2.关联数据库,查询对应的用户信息.
-        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+        //auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
 }
--- weiyun-user/user-service/src/main/java/com/wy/controller/IndexController.java
+++ weiyun-user/user-service/src/main/java/com/wy/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.wy.controller;
 
+import com.wy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String root() {
