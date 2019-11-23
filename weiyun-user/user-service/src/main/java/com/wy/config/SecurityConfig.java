319c40550ba9e880a29d90e3c16c83d2950d9d0a
yangyang <1023861154@qq.com>
2019-11-23 11:59:04 +0800

    登录页面完成,解决静态资源被拦截问题

--- weiyun-user/user-service/src/main/java/com/wy/config/SecurityConfig.java
+++ weiyun-user/user-service/src/main/java/com/wy/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userService;
 
+
     //对要访问的页面资源进行授权限制
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,8 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().
                     authorizeRequests()
                  //对页面|资源请求进行认证授权操作
-                .antMatchers("/dist/css/bootstrap.css", "/index")
+                .antMatchers("/favicon.ico","/fonts/**","/css/**","/js/**", "/index","/login")
                 .permitAll()
+                .anyRequest().authenticated()
                 .antMatchers("/user/**")
                 //.hasAuthority("ROLE_USER");
                 .hasAnyAuthority("ROLE_CAROWNER","ROLE_GOODSOWNER")
